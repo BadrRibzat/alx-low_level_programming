d4b8fe29f9d8eb03cab50628eef05d1d7a02c874
BadrRibzat <badrribzat@gmail.com>
2023-07-14 12:02:53 -0700

    permissions2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,6 +17,7 @@ int _strlen(char *s)
 		a++;
 	return (a);
 }
+
 /**
  * create_file - create file
  * @filename: name of the file
@@ -37,6 +38,6 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (len)
 		bytes = write(fd, text_content, len);
-	close (fd);
+	close(fd);
 	return (bytes == len ? 1 : -1);
 }
